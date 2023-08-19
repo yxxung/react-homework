97b42b2b45c155bc19c93c68e906e545d3562885
yxxung <loveus011@naver.com>
2023-08-20 02:40:59 +0900

    setting: 설정 업데이트

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'suit': ['SUIT Variable'],
-        'sans': ['GmarketSans'],
-        'pretendard': ['Pretendard Variable']
+        suit: ["SUIT Variable"],
       },
       colors: {
-        'gray1': '#ECECEC',
-        'gray2': '#4f4f4f',
-        'babyBlue': '#B3D0EB',
-        'babyBlue2': '#8DADCA'
-      }
+        gray1: "#EEEEEE",
+        gray2: "#4f4f4f",
+        babyBlue: "#B3D0EB",
+        babyBlue2: "#8DADCA",
+        babyBlue3: "#80B1DE",
+      },
+      screens: {
+        xs: {min: "390px", max: "640px"}
+      },
     },
   },
   plugins: [],
-}
+};
